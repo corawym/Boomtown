fcb230bee1d6027c36ee41f35219ad090fa45ce7
corawym <corawym@gmail.com>
2017-10-06 16:08:11 -0700

    Fetch data from db

--- client/src/containers/Cards/CardsContainer.js
+++ client/src/containers/Cards/CardsContainer.js
@@ -9,7 +9,12 @@ class CardsContainer extends Component {
   }
   fetchData = () => {
     const urls = [ 'http://localhost:3001/items', 'http://localhost:3001/users' ]
-    
+    Promise.all(urls.map(url =>
+      fetch(url).then(resp => resp.json())
+    )).then(data => {
+      
+      console.log (data)
+    })
   }
   render() {
     return (
