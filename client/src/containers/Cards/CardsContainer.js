62e623b0b47143998f2def7e8a87973ba1de4a0c
corawym <corawym@gmail.com>
2017-10-06 16:23:01 -0700

    Combined the items and users data into one list of object

--- client/src/containers/Cards/CardsContainer.js
+++ client/src/containers/Cards/CardsContainer.js
@@ -12,14 +12,28 @@ class CardsContainer extends Component {
     Promise.all(urls.map(url =>
       fetch(url).then(resp => resp.json())
     )).then(data => {
-      
-      console.log (data)
+      const [items, users] = data
+      const cardData = items.map(item => {
+        return {
+          "id": item.id,
+          "title": item.title,
+          "description": item.description,
+          "imageUrl": item.imageUrl,
+          "tags": item.tags,
+          "itemOwner": item.itemOwner,
+          "createdOn": item.createdOn,
+          "available": item.available,
+          "borrower": item.borrower,
+          "user": users.find(user => item.itemOwner === user.id)
+        }
+      })
+      this.setState({cardData})
     })
   }
   render() {
     return (
       <div className="cardsContainer">
-        <CardList/>
+        <CardList />
       </div>
     )
   }
