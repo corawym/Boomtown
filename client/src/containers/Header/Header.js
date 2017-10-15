f3694553f8a195ce7882ddb264dea80ad5d433ea
corawym <corawym@gmail.com>
2017-10-15 01:38:34 -0700

    Change to mobile first

--- client/src/containers/Header/Header.js
+++ client/src/containers/Header/Header.js
@@ -32,9 +32,9 @@ class Header extends Component {
         title="Title"
         iconElementLeft={ <Leftside filters={this.state.filters}  handleChange={this.handleChange} filterSelected={this.state.filterSelected}/> }
         iconElementRight={ <Rightside /> }
-        style={{ backgroundColor: '#fff', maxWidth: '1140px', margin: '0 auto', boxShadow: 'none', padding: '0 10px', display:'flex', alignItems:'center'}}
+        style={{ backgroundColor: '#fff', maxWidth: '1140px', margin: '0 auto', boxShadow: 'none', padding: '0 10px', display:'flex', alignItems:'center', flexWrap:'wrap'}}
         iconStyleLeft={{ margin: '0'}}
-        iconStyleRight={{ margin: '0'}}
+        iconStyleRight={{ marginRight: '0'}}
       />
     )
   }
--- client/src/containers/Header/Rightside.js
+++ client/src/containers/Header/Rightside.js
@@ -4,7 +4,7 @@ import { ButtonLogOut, ButtonMyProfile } from '../../components/common'
 class Rightside extends Component {
   render() {
     return (
-      <div>
+      <div className="headerRightWrapper">
         <ButtonMyProfile />
         <ButtonLogOut />
       </div>
