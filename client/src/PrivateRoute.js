38507e5dab1c98904c3b08b063969c6bac7019ce
corawym <corawym@gmail.com>
2017-11-29 17:08:23 -0800

    Add a loader on sign in page

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import CircularProgress from 'material-ui/CircularProgress';
 
-const PrivateRoute = ({ component: Component, user, ...rest }) => (
-  <Route
-    {...rest}
-    location={rest.location}
-    render={props => (
-      user ? (
-        <Component {...props} />
-      )
-      : (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />)
-    )}
-  />
-);
+const PrivateRoute = ({ component: Component, user, auth, ...rest }) => {
+  if ( auth !== null ) {
+    console.log('done')
+    return(
+      
+      <Route
+        {...rest}
+        location={rest.location}
+        render={props => (
+          user ? (
+            <Component {...props} />
+          )
+          : (<Redirect to={{ pathname: '/login', state: { from: props.location } }} />)
+        )}
+      />
+    )  
+  } else {
+    console.log('loading')
+    return(
+      <CircularProgress size={60} thickness={7} />
+    )
+  }
+};
 
 export default connect (state => {
   return {
